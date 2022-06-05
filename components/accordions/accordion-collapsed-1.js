644383bc2f0c07115c4bfedfd780f7b30a5c2bef
Miguel J Medina <pana9422@gmail.com>
2022-06-04 22:25:12 -0500

    data of test

---
+++ components/accordions/accordion-collapsed-1.js
@@ -0,0 +1,9 @@
+const toggleAccordion = e => {
+    e.currentTarget.parentElement.classList.toggle('accordion--collapsed');
+ }
+ 
+ const accordions_group = document.querySelectorAll('.accordion__header');
+ 
+ accordions_group.forEach(accordion => {
+    accordion.addEventListener('click', toggleAccordion);
+ });
