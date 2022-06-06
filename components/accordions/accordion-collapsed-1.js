a50f021f5b638fb14399873ab80e4f8e5bd32337
Miguel J Medina <pana9422@gmail.com>
2022-06-06 15:40:59 -0500

    autorun function is reemoved of component accordion-collapsed-1

--- components/accordions/accordion-collapsed-1.js
+++ components/accordions/accordion-collapsed-1.js
@@ -1,11 +1,9 @@
-( function(){
-   const toggleAccordion = e => {
-      e.currentTarget.parentElement.classList.toggle('accordion--collapsed');
-   }
-   
-   const accordions_group = document.querySelectorAll('.accordion__header');
-   
-   accordions_group.forEach(accordion => {
-      accordion.addEventListener('click', toggleAccordion);
-   });
-})()
+const toggleAccordion = e => {
+   e.currentTarget.parentElement.classList.toggle('accordion--collapsed');
+}
+
+const accordions_group = document.querySelectorAll('.accordion__header');
+
+accordions_group.forEach(accordion => {
+   accordion.addEventListener('click', toggleAccordion);
+});
