345402db9429884b8e16aa0f986bdba9efda52e7
Miguel J Medina <pana9422@gmail.com>
2022-06-08 22:45:59 -0500

    creating of component cards, buttons, breadcrumb, badge and avatar in HTML CSS & JS

---
+++ components/buttons/button-6.js
@@ -0,0 +1,14 @@
+const $button = document.querySelectorAll(".btn")
+
+$button.forEach( btn => {
+    btn.addEventListener( "click", () => {
+        const $icon = btn.querySelector(".btn__icon")
+        btn.classList.add("btn--loading")
+        $icon.classList.replace("fa-link", "fa-spinner")
+    
+        setTimeout(() => {
+            btn.classList.remove("btn--loading")
+            $icon.classList.replace("fa-spinner", "fa-link")
+        }, 5000);
+    })
+});
---
+++ components/buttons/button-dropdown-7.js
@@ -0,0 +1,11 @@
+const btnDropdown = document.querySelector('.btn--dropdown');
+const dropdownList = document.querySelector('.btn--dropdown .btn__dropdown');
+
+// Functions
+const toggleState = () => {
+  btnDropdown.classList.toggle('dropdown-opened');
+  dropdownList.classList.toggle('btn__dropdown--unfolded');
+}
+
+// Code execution
+btnDropdown.addEventListener('click', toggleState);
