57ba61765b3768bbea659688d8171edec31be889
Miguel J Medina <pana9422@gmail.com>
2022-06-21 10:51:13 -0500

    removed change iconof fontawesome

--- components/buttons/button-6.js
+++ components/buttons/button-6.js
@@ -4,11 +4,9 @@ $button.forEach((btn) => {
     btn.addEventListener("click", () => {
         const $icon = btn.querySelector(".btn__icon");
         btn.classList.add("btn--loading");
-        $icon.classList.replace("fa-link", "fa-spinner");
 
         setTimeout(() => {
             btn.classList.remove("btn--loading");
-            $icon.classList.replace("fa-spinner", "fa-link");
         }, 5000);
     });
 });
