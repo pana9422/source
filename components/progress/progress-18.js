077fe0e9fe9c6ec473da1b8cbbc1215054bc3dee
Miguel J Medina <pana9422@gmail.com>
2022-06-26 19:13:40 -0500

    add component progress

---
+++ components/progress/progress-18.js
@@ -0,0 +1,23 @@
+const progressBar = document.querySelector('.progress-bar');
+const progress = document.createElement('SPAN');
+
+progress.className = 'progress-bar__progress';
+progressBar.appendChild(progress);
+
+
+let interval;
+let width = 0;
+
+const move = () => {
+  if(width > 100) {
+    clearInterval(interval);
+    interval = null;
+  } else {
+    progress.style.width = `${width}%`;
+    width++;
+  }
+}
+
+if (!interval) {
+  interval = setInterval(move, 100);
+}
