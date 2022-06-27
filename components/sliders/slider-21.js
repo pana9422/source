44ba075032cb4c145d55a0e2fbd035765ee658df
Miguel J Medina <pana9422@gmail.com>
2022-06-27 14:20:07 -0500

    update custom property of component slider

--- components/sliders/slider-21.js
+++ components/sliders/slider-21.js
@@ -4,7 +4,7 @@ sliders.forEach(slider => {
 
     slider.addEventListener("input", () => {
         let val = ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
-        slider.style.backgroundImage = `linear-gradient(to right, var(--france-500) ${val}%, var(--france-100) ${val}%`;
+        slider.style.backgroundImage = `linear-gradient(to right, #1b67ad ${val}%, #9acaf7 ${val}%`;
     });
 
 });
