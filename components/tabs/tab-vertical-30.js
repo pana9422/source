7e00247a6a4ea56d62b31f5f8f08f7c6d93b61fc
Miguel J Medina <pana9422@gmail.com>
2022-06-28 19:10:27 -0500

    update component tab-vertical

--- components/tabs/tab-vertical-30.js
+++ components/tabs/tab-vertical-30.js
@@ -1,47 +1,47 @@
 //=== Functions ===//
 const toggleBtnVisibility = (isHorizontal) => {
     // toggle button visivility according to TAB orientation
-    if (isHorizontal) {
-        (tabs_h.scrollLeft > 0)
-            ? backBtn_h.classList.add('tab__btn--visible')
-            : backBtn_h.classList.remove('tab__btn--visible');
-
-        (tabs_h.scrollLeft >= tabs_h.scrollWidth - tabs_h.offsetWidth)
-            ? nextBtn_h.classList.remove('tab__btn--visible')
-            : nextBtn_h.classList.add('tab__btn--visible');
+    if(isHorizontal) {
+      (tabs_h.scrollLeft > 0) 
+        ? backBtn_h.classList.add('tab__btn--visible')
+        : backBtn_h.classList.remove('tab__btn--visible');
+  
+      (tabs_h.scrollLeft >= tabs_h.scrollWidth - tabs_h.offsetWidth) 
+        ? nextBtn_h.classList.remove('tab__btn--visible')
+        : nextBtn_h.classList.add('tab__btn--visible');
     } else {
-        (tabs_v.scrollTop > 0)
-            ? backBtn_v.classList.add('tab__btn--visible')
-            : backBtn_v.classList.remove('tab__btn--visible');
-
-        (tabs_v.scrollTop >= tabs_v.scrollHeight - tabs_v.offsetHeight)
-            ? nextBtn_v.classList.remove('tab__btn--visible')
-            : nextBtn_v.classList.add('tab__btn--visible');
+      (tabs_v.scrollTop > 0) 
+        ? backBtn_v.classList.add('tab__btn--visible')
+        : backBtn_v.classList.remove('tab__btn--visible');
+  
+      (tabs_v.scrollTop >= tabs_v.scrollHeight - tabs_v.offsetHeight) 
+        ? nextBtn_v.classList.remove('tab__btn--visible')
+        : nextBtn_v.classList.add('tab__btn--visible');
     }
-}
-const verticalTabFunc = () => {
-    // Functionality for the vertical tab
-    toggleBtnVisibility(false);
-
-    nextBtn_v.addEventListener('click', () => {
-        tabs_v.scrollTop += tabs_v.scrollHeight;
+  }
+  
+  const horizontalTabFunc = () => {
+    // Functionality for the horizontal tab
+    toggleBtnVisibility(true);
+  
+    nextBtn_h.addEventListener('click', () => {
+      tabs_h.scrollLeft += tabs_h.scrollWidth;
     });
-
-    backBtn_v.addEventListener('click', () => {
-        tabs_v.scrollTop -= tabs_v.scrollHeight;
+  
+    backBtn_h.addEventListener('click', () => {
+      tabs_h.scrollLeft -= tabs_h.scrollWidth;
     });
-
-    tabs_v.addEventListener('scroll', () => {
-        toggleBtnVisibility(false);
+  
+    tabs_h.addEventListener('scroll', () => {
+      toggleBtnVisibility(true);
     });
-}
-
-//=== Variables ===//
-const tabs_v = document.querySelector('.tab--v .tab__tabs');
-const nextBtn_v = document.querySelector('.tab--v .tab__nextBtn');
-const backBtn_v = document.querySelector('.tab--v .tab__backBtn');
-
-//=== Code execution ===//
-
-if (tabs_v) verticalTabFunc();
-else console.log('There is no tab element');
+  }
+  
+  //=== Variables ===//
+  const tabs_h = document.querySelector('.tab--h .tab__tabs');
+  const nextBtn_h = document.querySelector('.tab--h .tab__nextBtn');
+  const backBtn_h = document.querySelector('.tab--h .tab__backBtn');
+  
+  //=== Code execution ===//
+  if (tabs_h) horizontalTabFunc();
+  else console.log('There is no tab element');
